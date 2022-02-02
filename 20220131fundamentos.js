ac8dda5163dc6e277dc40bb8c79dbb1865833d1a
elvisrodriguezc <elvisrodriguezc@gmail.com>
2022-02-02 00:23:42 -0500

    Academlo Primeros archivos

---
+++ 20220131fundamentos.js
@@ -0,0 +1,4 @@
+console.log("Bien");
+// todo: comentar
+var i = 0;
+console.log(i);
---
+++ 20220201fundamentos.js
@@ -0,0 +1,23 @@
+/* let variableConLet = 'jesus'
+variableConLet = 'Brandom'
+let variableConComillasDobles = "Hola"
+variableConComillasDobles = 'Hola Mundo'
+variableConComillasDobles = `Hola pERU`
+console.log(variableConLet);
+console.log(variableConComillasDobles);
+console.log(typeof variableConComillasDobles)
+const VARIABLE_CONSTANTE = 'Hola'
+console.log(VARIABLE_CONSTANTE)
+const VARIABLE = 10
+console.log(String(VARIABLE))
+console.log(Boolean(0)) */
+
+let variableNombre = 'Elvis'
+variableNombre = "Rodríguez"
+console.log('Valor de la Variable: ' + variableNombre)
+for (var i = 0; i < 5; i++) {
+    console.log(`aqui se ve let ${i}`)
+}
+console.log('esto dara error')
+console.log(`Valor final de i: ${i}`)
+console.log(i)
